e6985a1f9fb6c5a650bf36588d007d22dd399a6b
tuanthanh <marbled.thanh.tt@gmail.com>
2022-08-30 16:47:17 +0700

    edit title

---
+++ example/data.js
@@ -0,0 +1,39 @@
+var ganttData = [
+	{
+		id: 1, name: "không biết", series: [
+			{ name: "1", start: new Date(), end: new Date() },
+			{ name: "2", start: new Date(), end: new Date(), color: "#f0f0f0" }
+		]
+	}, 
+	{
+		id: 2, name: "không biết", series: [
+			{ name: "1", start: new Date(), end: new Date() },
+			{ name: "2", start: new Date(), end: new Date(), color: "#f0f0f0" },
+		]
+	}, 
+	{
+		id: 3, name: "không biết", series: [
+			{ name: "1", start: new Date(), end: new Date() },
+			{ name: "2", start: new Date(), end: new Date(), color: "#f0f0f0" }
+		]
+	}, 
+	{
+		id: 4, name: "không biết", series: [
+			{ name: "1", start: new Date(), end: new Date() },
+			{ name: "2", start: new Date(), end: new Date(), color: "#f0f0f0" }
+		]
+	},
+	{
+		id: 5, name: "không biết", series: [
+			{ name: "1", start: new Date(), end: new Date() },
+			{ name: "2", start: new Date(), end: new Date(), color: "#f0f0f0" }
+		]
+	}, 
+	{
+		id: 6, name: "không biết", series: [
+			{ name: "1", start: new Date(), end: new Date() },
+			{ name: "2", start: new Date(), end: new Date(), color: "#f0f0f0" },
+		]
+	}, 
+	
+];
